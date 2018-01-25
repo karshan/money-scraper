65750e279a40f256c78d434129eaff7d05de37a7
Karshan Sharma <karshan.sharma@gmail.com>
2018-01-24 17:33:52 -0800

    serverify

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const chaseScraper = require('./chase-scraper');
+const http = require('http');
 
-var p = chaseScraper.scrape(require('./creds'));
-p.then((r) => {
-  if (r.ok) {
-    console.log(JSON.stringify(r.downloadedFiles, null, 2));
-  } else {
-    console.log(JSON.stringify(r, null, 2));
-  }
+const server = http.createServer((req, res) => {
+  // TODO check request path
+  // TODO get creds from request
+  var p = chaseScraper.scrape(require('./creds'));
+  p.then((r) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(r, null, 2));
+  });
 });
+
+server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(8000);
+console.log('listening on 8000');
